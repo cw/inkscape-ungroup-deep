b87f2214d73f6c200d2c8f8392fc7af4ee6423e3
Matěj Cepl <mcepl@redhat.com>
2012-01-03 22:54:14 +0100

    Added logging

--- ungroup_deep.py
+++ ungroup_deep.py
@@ -9,13 +9,16 @@ for an exmaple how to do the transform of parent to children.
 """
 
 from __future__ import division, print_function
+import logging
+logging.basicConfig(format='%(levelname)s:%(funcName)s:%(message)s',
+    level=logging.DEBUG)
 
 try:
     import inkex
     #from inkex import unittouu
 except ImportError:
     raise ImportError("""No module named inkex in {0}.""".format(__file__))
-from svg_utils import propagate_attribs
+from ungroup_utils import propagate_attribs
 
 SVG_NS = "http://www.w3.org/2000/svg"
 INKSCAPE_NS = "http://www.inkscape.org/namespaces/inkscape"
@@ -23,18 +26,23 @@ INKSCAPE_NS = "http://www.inkscape.org/namespaces/inkscape"
 
 class Ungroup(inkex.Effect):
     def _ungroup(self, obj):
-        propagate_attribs(obj)
+        logging.debug("obj = %s", obj)
+
         if (obj.tag == inkex.addNS('g', 'svg')):
+            propagate_attribs(obj)
+            logging.debug("group element = %s", obj)
             children = list(obj)
             obj_parent = obj.getparent()
             obj_index = list(obj_parent).index(obj)
             obj_parent.replace(obj, children[0])
-            if len(children) > 1:
-                children = children[1:]
-                for child in reversed(children):
-                    obj_parent.insert(obj_index, child)
+            for child in reversed(children[1:]):
+                obj_parent.insert(obj_index, child)
+
             for elem in children:
                 self._ungroup(elem)
+
+        else:
+            logging.debug("non-group element = %s", obj)
 
     def effect(self):
         if len(self.selected):
